e8a469a9305a907a6486d2299c37917699c5d7d4
Ted Teah <from@dabronx.nyc>
2021-07-04 14:24:14 -0400

    added api return

---
+++ src/views/Artists.vue
@@ -0,0 +1,36 @@
+
+
+<template>
+  <div class="foo">
+hkldfkjl;skljdgk
+
+ </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from 'axios';
+export default {
+  name: 'Artists',
+  components: {
+    
+  },
+
+ data() {
+    return {
+      posts: [],
+      errors: []
+    }
+  },
+
+  // Fetches posts when the component is created.
+  async created() {
+    try {
+      const response = await axios.get(`https://spreadsheets.google.com/feeds/list/18kC36XlinBoxImrravjSceOXChOg4fOBW0_W0ECrWZw/1/public/full?alt=json`)
+      this.posts = response.data.feed.entry.gs
+    } catch (e) {
+      this.errors.push(e)
+    }
+  }
+}
+</script>
